9d5fe2061f6221ff9098ffd838da64f32bb3b8dc
Azrin <mdazrin.programmer@gmail.com>
2023-03-11 18:41:01 +0800

    Modify Core Test
    Add code to test inertia pages

--- tests/Feature/CoreTest.php
+++ tests/Feature/CoreTest.php
@@ -1,13 +1,22 @@
 <?php
 
 use App\Models\User;
+use Inertia\Testing\AssertableInertia as Assert;
 
-it('can surf Core index', function (){
+it('can surf Core table', function (){
     $user = User::factory()->create();
 
     $response = $this
         ->actingAs($user)
         ->get('/');
 
-    $response->assertOk();
+    $response->assertInertia(fn (Assert $page) => $page
+        // Checking a root-level property...
+        ->component('Welcome')
+        ->has('cores')
+
+    );
+
+
+    $response->assertStatus(200);
 });
