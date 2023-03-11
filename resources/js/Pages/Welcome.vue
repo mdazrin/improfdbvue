2447e6c9e8e28896b63713b9d2bee262a633c9db
Azrin <mdazrin.programmer@gmail.com>
2023-03-11 20:42:35 +0800

    Update Core table

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -17,12 +17,46 @@ defineProps({
         </title>
     </Head>
     <h1>Welcome</h1>
-    <li v-for="core in cores">
-        {{ core.id }}
-        {{ core.first_name }}
-        {{ core.last_name }}
-        {{ core.ppi }}
-    </li>
+    <table class="table-fixed mx-auto mt-10">
+        <thead class="bg-blue-100">
+        <tr>
+            <td class="px-6 py-6">
+                Id
+            </td>
+            <td class="px-6 py-6">
+                First Name
+            </td>
+            <td class="px-6 py-6">
+                Last Name
+            </td>
+            <td class="px-6 py-6">
+                PPI
+            </td>
+            <td class="px-6 py-6">
+                Batch
+            </td>
+        </tr>
+        </thead>
+        <tbody class="border-b">
+        <tr v-for="core in cores">
+            <td class="px-6 py-6">
+                {{core.id}}
+            </td>
+            <td class="px-6 py-6">
+                {{core.first_name}}
+            </td>
+            <td class="px-6 py-6">
+                {{core.last_name}}
+            </td>
+            <td class="px-6 py-6">
+                {{core.ppi}}
+            </td>
+            <td class="px-6 py-6">
+                {{core.batch}}
+            </td>
+        </tr>
+        </tbody>
+    </table>
 
 
 </template>
