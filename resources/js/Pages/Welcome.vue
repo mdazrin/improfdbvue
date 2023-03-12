f8ed0b829b0a44d633118e2c017134f56c4ed642
Azrin <mdazrin.programmer@gmail.com>
2023-03-13 02:32:27 +0800

    Sort table

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -1,12 +1,39 @@
 <script setup>
 import {Head} from "@inertiajs/vue3";
 import Layout from '@/Layouts/Layout.vue'
+import {computed, ref} from "vue";
 
 defineOptions({ layout: Layout })
-defineProps({
+const tableData = defineProps({
     cores:Object,
 
 })
+
+const sortColumn = ref('name');
+const sortDirection = ref('asc');
+
+const sortedTableData = computed(() => {
+    const data = [...tableData.cores];
+    data.sort((a, b) => {
+        if (sortDirection.value === 'asc') {
+            return a[sortColumn.value] > b[sortColumn.value] ? 1 : -1;
+        } else {
+            return a[sortColumn.value] < b[sortColumn.value] ? 1 : -1;
+        }
+    });
+    return data;
+});
+
+const updateSort = (column) => {
+    if (column === sortColumn.value) {
+        sortDirection.value = sortDirection.value === 'asc' ? 'desc' : 'asc';
+    } else {
+        sortColumn.value = column;
+        sortDirection.value = 'asc';
+    }
+};
+
+
 
 </script>
 
@@ -20,25 +47,25 @@ defineProps({
     <table class="table-fixed mx-auto mt-10">
         <thead class="bg-blue-100">
         <tr>
-            <td class="px-6 py-6">
+            <td @click="updateSort('id')" class="px-6 py-6">
                 Id
             </td>
-            <td class="px-6 py-6">
+            <td @click="updateSort('first_name')" class="px-6 py-6">
                 First Name
             </td>
-            <td class="px-6 py-6">
+            <td @click="updateSort('last_name')" class="px-6 py-6">
                 Last Name
             </td>
-            <td class="px-6 py-6">
+            <td @click="updateSort('ppi')" class="px-6 py-6">
                 PPI
             </td>
-            <td class="px-6 py-6">
+            <td @click="updateSort('batch')" class="px-6 py-6">
                 Batch
             </td>
         </tr>
         </thead>
         <tbody class="border-b">
-        <tr v-for="core in cores">
+        <tr v-for="core in sortedTableData">
             <td class="px-6 py-6">
                 {{core.id}}
             </td>
