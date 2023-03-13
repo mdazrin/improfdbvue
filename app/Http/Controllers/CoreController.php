7b33517c841938431b7d0de5f4d5829fc1a51e4d
Azrin <mdazrin.programmer@gmail.com>
2023-03-13 13:03:46 +0800

    Model SPA Security

--- app/Http/Controllers/CoreController.php
+++ app/Http/Controllers/CoreController.php
@@ -14,7 +14,15 @@ class CoreController extends Controller
      */
     public function index(): Response
     {
-        $cores = Core::all();
+        $cores = Core::all()->map(fn($core)=>[
+            'id'=>$core->id,
+            'first_name'=>$core->first_name,
+            'last_name'=>$core->last_name,
+            'ppi'=>$core->ppi,
+            'batch'=>$core->batch
+        ]);
+
+
         return Inertia::render('Welcome',[
             'cores'=>$cores,
         ]);
