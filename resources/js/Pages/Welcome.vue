6717ef1f71ba9c1c7a3599cf80bf4e860f208c41
Azrin <mdazrin.programmer@gmail.com>
2023-03-13 21:50:38 +0800

    Rebase
    Remove old codes

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -1,21 +1,59 @@
 <script setup>
-import Grid from '@/Components/Grid.vue'
-import { ref } from 'vue'
 
-const searchQuery = ref('')
-const gridColumns = ['id', 'first_name','last_name','ppi','batch']
-const gridData = defineProps({
+defineProps({
     cores:Object
 })
 </script>
 
 <template>
-    <form id="search">
-        Search <input name="query" v-model="searchQuery">
-    </form>
-    <Grid
-        :data="gridData.cores"
-        :columns="gridColumns"
-        :filter-key="searchQuery">
-    </Grid>
+    <Head>
+        <title>
+            My App
+        </title>
+    </Head>
+    <h1>Welcome</h1>
+    <table class="table-fixed mx-auto mt-10">
+        <thead class="bg-blue-100">
+        <tr>
+            <td class="px-6 py-6">
+                Id
+            </td>
+            <td class="px-6 py-6">
+                First Name
+            </td>
+            <td class="px-6 py-6">
+                Last Name
+            </td>
+            <td class="px-6 py-6">
+                PPI
+            </td>
+            <td class="px-6 py-6">
+                Batch
+            </td>
+        </tr>
+        </thead>
+        <tbody class="border-b">
+        <tr v-for="core in cores" :key="core.id">
+            <td class="px-6 py-6">
+                {{core.id}}
+            </td>
+            <td class="px-6 py-6">
+                {{core.first_name}}
+            </td>
+            <td class="px-6 py-6">
+                {{core.last_name}}
+            </td>
+            <td class="px-6 py-6">
+                {{core.ppi}}
+            </td>
+            <td class="px-6 py-6">
+                {{core.batch}}
+            </td>
+        </tr>
+        </tbody>
+    </table>
+
+
+
+
 </template>
