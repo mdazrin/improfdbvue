4c4a4ddf036e5ee3d5b0597985f7955dd1228874
Azrin <mdazrin.programmer@gmail.com>
2023-03-14 00:15:34 +0800

    User and Core relationship

--- app/Models/Core.php
+++ app/Models/Core.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Core extends Model
 {
@@ -11,5 +12,10 @@ class Core extends Model
 
     public static function where(string $string, int $int)
     {
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
     }
 }
--- database/migrations/2023_03_06_221638_create_cores_table.php
+++ database/migrations/2023_03_06_221638_create_cores_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('cores', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
             $table->timestamps();
             $table->string('first_name');
             $table->string('last_name');
